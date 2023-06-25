a7cb5dc2249f4d589639ae5f149ae7a85c519259
dorume-abstract <savitskiy.danilo.everything@gmail.com>
2023-06-25 14:43:42 +0300

    Csv -> excel and vice versa

--- server/backend/apps/converter/csv2excel.py
+++ server/backend/apps/converter/csv2excel.py
@@ -1,6 +1,20 @@
 from .base.abstract_converter import FileConverter
-
+import csv
+from openpyxl import Workbook
 
 class CsvToExcel(FileConverter):
     async def convert(from_path, to_path) -> None:
-        pass
+        with open(from_path, 'r') as csv_file:
+          csv_reader = csv.reader(csv_file)
+          data = list(csv_reader)
+
+      # Create a new Excel workbook
+        workbook = Workbook()
+        sheet = workbook.active
+
+        # Write the data from CSV to the Excel sheet
+        for row in data:
+            sheet.append(row)
+
+        # Save the workbook as an Excel file
+        workbook.save(to_path)
--- server/backend/apps/converter/excel2csv.py
+++ server/backend/apps/converter/excel2csv.py
@@ -1,6 +1,21 @@
 from .base.abstract_converter import FileConverter
-
+import openpyxl
+import csv
 
 class ExcelToCsv(FileConverter):
     async def convert(from_path, to_path) -> None:
-        pass
+        # Open the Excel file
+      workbook = openpyxl.load_workbook(from_path)
+
+      # Get the active sheet
+      sheet = workbook.active
+
+      # Read the data from the Excel sheet
+      data = []
+      for row in sheet.iter_rows(values_only=True):
+          data.append(row)
+
+      # Write the data to a CSV file
+      with open(to_path, 'w', newline='') as csv_file:
+          csv_writer = csv.writer(csv_file)
+          csv_writer.writerows(data)
